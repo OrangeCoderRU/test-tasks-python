05c74257cb7d1c0db7a7b910c922beec21bf621a
Max Sablin <max.sablin@mail.ru>
2020-08-22 20:05:30 +0300

    Create task1.py

---
+++ task1/task1.py
@@ -0,0 +1,29 @@
+import sys
+
+def itoBase(nb, base):
+    list = []
+    num_base = len(base)
+    while nb != 0:
+        if nb == 1:
+            list.append(1)
+        else:
+            list.append(int(nb) % num_base)
+        nb = int(nb) // int(num_base)
+    list.reverse()
+    print( "".join("{0}".format(n) for n in list))
+
+def usage():
+    """Шаблон параметров командной строки"""
+    usage = "python3 task.1py num base"
+    print(usage)
+
+def main():
+    if len(sys.argv) == 3:
+        itoBase(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 4:
+        itoBase(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        usage()
+
+if __name__ == "__main__":
+    main()
